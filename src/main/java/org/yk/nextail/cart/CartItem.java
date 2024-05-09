651502aa411a423d8cfcf247bbfc29b5afe7092b
Jonatan Soto <jonatan_soto@hotmail.com>
2024-05-09 14:44:50 +0200

    Improved class: CartItem now has all fields set as final + + added a constructor with all fields + removed setters

--- src/main/java/org/yk/nextail/cart/CartItem.java
+++ src/main/java/org/yk/nextail/cart/CartItem.java
@@ -2,31 +2,25 @@ package org.yk.nextail.cart;
 
 public class CartItem {
 
-    private String code;
-    private String name;
-    private double price;
+    private final String code;
+    private final String name;
+    private final Double price;
+
+    public CartItem(String code, String name, Double price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
 
     public String getCode() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
+    public Double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 }
