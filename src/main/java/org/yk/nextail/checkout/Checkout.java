dca89c32e9da6afe03e704c1af03f4364a801242
Jonatan Soto <jonatan_soto@hotmail.com>
2024-05-09 16:00:05 +0200

    Improved class: implemented scan method in Checkout that accepts cartItem

--- src/main/java/org/yk/nextail/checkout/Checkout.java
+++ src/main/java/org/yk/nextail/checkout/Checkout.java
@@ -1,10 +1,25 @@
 package org.yk.nextail.checkout;
 
+import org.yk.nextail.cart.CartItem;
 import org.yk.nextail.price.PriceRule;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Checkout {
 
-    public Checkout(PriceRule pricesRule) {
+    private final List<CartItem> cartItems;
 
+    public Checkout(PriceRule pricesRule) {
+        this.cartItems = new ArrayList<>();
     }
+
+    public void scan(CartItem cartItem) {
+        cartItems.add(cartItem);
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
 }
