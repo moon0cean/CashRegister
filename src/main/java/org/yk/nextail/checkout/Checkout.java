c1736c9b14c7d3473291fafbe9a28a056d25f24c
Jonatan Soto <jonatan_soto@hotmail.com>
2024-05-10 17:15:49 +0200

    Improved class: Checkout class includes its first approach for the condition evaluation implementation. Added the contract for the action application logic. Also, a bit of refactor + added logger

--- src/main/java/org/yk/nextail/checkout/Checkout.java
+++ src/main/java/org/yk/nextail/checkout/Checkout.java
@@ -1,21 +1,25 @@
 package org.yk.nextail.checkout;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yk.nextail.cart.CartItem;
 import org.yk.nextail.price.PricingRule;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Checkout {
-
+    private static final Logger LOG = LoggerFactory.getLogger(Checkout.class);
     private final List<CartItem> cartItems;
-
-    private final PricingRule priceRule;
+    private final List<PricingRule> pricingRules;
 
     // TODO: Likely to be redesigned in order to not have to pass pricing rules at this stage
-    public Checkout(PricingRule pricesRule) {
+    public Checkout(List<PricingRule> pricingRules) {
         this.cartItems = new ArrayList<>();
-        this.priceRule = pricesRule;
+        this.pricingRules = pricingRules;
     }
 
     public void scan(CartItem cartItem) {
@@ -28,8 +32,63 @@ public class Checkout {
     }
 
     public Double getCartTotal() {
-        // FIXME: Apply price rules
+        if (evaluatePricingRuleConditions()) {
+            // FIXME: Apply pricing rules actions
+            applyPricingRuleActions();
+        }
         return cartItems.stream().mapToDouble(ci -> ci.getPrice()).sum();
+    }
+
+    /**
+     * Evaluates pricing rule conditions against current cart items.
+     * NOTE: currently there's a limitation for composed conditions, which are being evaluated using AND operator
+     *
+     * @return
+     */
+    private boolean evaluatePricingRuleConditions() {
+        Arrays.stream(PricingRule.PriceRuleCondition.PriceRuleConditionType.values()).forEach(prc -> {
+//            LOG.info("PRC: " + prc);
+        });
+
+        Optional<PricingRule.PriceRuleCondition<?>> productCodeConditionOptional = pricingRules.stream()
+                .flatMap(pr -> pr.getConditions().stream())
+                .filter(c -> c.getConditionType().equals(PricingRule.PriceRuleCondition.PriceRuleConditionType.PRODUCT_CODE))
+                .findFirst();
+
+        List<CartItem> filteredCartItems = cartItems;
+
+        // 1. Apply first PRODUCT_CODE condition when present
+        if (productCodeConditionOptional.isPresent()) {
+            LOG.info("Found PRODUCT_CODE condition, evaluating...");
+            PricingRule.PriceRuleCondition<?> productCodeCondition = productCodeConditionOptional.get();
+            LOG.info(productCodeCondition.toString());
+
+            // FIXME: Honor condition operator
+            filteredCartItems = cartItems.stream()
+                    .filter(ci -> productCodeCondition.getConditionValue().equals(ci.getCode()))
+                    .collect(Collectors.toList());
+
+        }
+
+        Optional<PricingRule.PriceRuleCondition<?>> quantityInCheckoutConditionOptional = pricingRules.stream()
+                .flatMap(pr -> pr.getConditions().stream())
+                .filter(c -> c.getConditionType().equals(PricingRule.PriceRuleCondition.PriceRuleConditionType.PRODUCT_QUANTITY_IN_CHECKOUT))
+                .findFirst();
+        if (quantityInCheckoutConditionOptional.isPresent()) {
+            LOG.info("Found PRODUCT_QUANTITY_IN_CHECKOUT condition, evaluating...");
+            PricingRule.PriceRuleCondition<?> quantityInCheckoutCondition = quantityInCheckoutConditionOptional.get();
+            if ((Integer) quantityInCheckoutCondition.getConditionValue() == filteredCartItems.size()) {
+                return true;
+            }
+            LOG.info(quantityInCheckoutCondition.toString());
+        }
+
+//        pricingRules.stream().filter(pr -> pr.getConditions().stream().filter()
+        return false;
+    }
+
+    private void applyPricingRuleActions() {
+
     }
 
 }
